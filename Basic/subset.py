5ab78ff17ffd40c82c85b3ca058323228bfc2c8b
officialpradip <study.pradipthapa@gmail.com>
2023-05-28 11:40:16 +0545

    subset

--- Basic/subset.py
+++ Basic/subset.py
@@ -1,3 +1,8 @@
 import pandas as pd
-demo=pd.read_csv("./train.csv")
+demo = pd.read_csv("./train.csv")
 print(demo.head(5))
+
+# select specific colums
+ages = demo[["Age", "Sex"]]
+print(ages)
+print(ages.shape)
